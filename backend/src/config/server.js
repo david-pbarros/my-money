bf763783e1f4e54afa9a51dec529c5145d8db5ef
David Barros <david_pbarros@hotmail.com>
2019-03-06 17:07:49 -0300

    Backend OK

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,12 @@
 const port = 3004;
 
 const bodyParser = require('body-parser');
-const express = require('express');
-
-const server = express();
+const server = require('express')();
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
+server.use(require('./cors'));
+server.use(require('express-query-int')());//Para tratar parametros de query tranformado em numerico quando necessario
 
 server.listen(port, function() {
     console.log(`rodando na porta ${port}`);
